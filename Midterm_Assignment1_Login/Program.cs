62d5fa050ec4bc1cf319aa4c70f628ea30e35a05
madieson sumbingco <sumbingcomadie@gmail.com>
2024-04-13 01:31:54 +0800

    Homepage UI

--- Midterm_Assignment1_Login/Program.cs
+++ Midterm_Assignment1_Login/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Register}/{id?}"); // Corrected controller name
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
