9ae84249ba01442a05c93e3a4896824d86e35c43
madieson sumbingco <sumbingcomadie@gmail.com>
2024-04-13 00:58:53 +0800

    Register form UI

--- Midterm_Assignment1_Login/Program.cs
+++ Midterm_Assignment1_Login/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}"); // Corrected controller name
+    pattern: "{controller=Account}/{action=Register}/{id?}"); // Corrected controller name
 
 app.Run();
